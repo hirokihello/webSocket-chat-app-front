b6dda25f1bb30e5edbece423b02c6741d25c8713
hirokihello <iammyeye1@gmail.com>
2020-08-16 23:37:37 +0900

    Feature: add editor config

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: run
+	run: npm run serve
+
+.PHONY: install
+	install: npm run install
--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,57 @@
 <template>
-  <main id="app">
-    <div class="row">
-        <div class="col s12">
-            <div class="card horizontal">
-                <div id="chat-messages" class="card-content" v-html="chatContent">
+    <main id="app">
+        <div class="row">
+            <div class="col s12">
+                <div class="card horizontal">
+                    <div id="chat-messages" class="card-content" v-html="chatContent">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row" v-if="joined">
-        <div class="input-field col s8">
-            <input type="text" v-model="newMsg" @keyup.enter="send">
+        <div class="row" v-if="joined">
+            <div class="input-field col s8">
+                <input type="text" v-model="newMsg" @keyup.enter="send">
+            </div>
+            <div class="input-field col s4">
+                <button class="waves-effect waves-light btn" @click="send">
+                    <i class="material-icons right">chat</i>
+                    Send
+                </button>
+            </div>
         </div>
-        <div class="input-field col s4">
-            <button class="waves-effect waves-light btn" @click="send">
-                <i class="material-icons right">chat</i>
-                Send
-            </button>
+        <div class="row" v-if="!joined">
+            <div class="input-field col s8">
+                <input type="email" v-model.trim="email" placeholder="Email">
+            </div>
+            <div class="input-field col s8">
+                <input type="text" v-model.trim="username" placeholder="Username">
+            </div>
+            <div class="input-field col s4">
+                <button class="waves-effect waves-light btn" @click="join()">
+                    <i class="material-icons right">done</i>
+                    Join
+                </button>
+            </div>
         </div>
-    </div>
-    <div class="row" v-if="!joined">
-        <div class="input-field col s8">
-            <input type="email" v-model.trim="email" placeholder="Email">
-        </div>
-        <div class="input-field col s8">
-            <input type="text" v-model.trim="username" placeholder="Username">
-        </div>
-        <div class="input-field col s4">
-            <button class="waves-effect waves-light btn" @click="join()">
-                <i class="material-icons right">done</i>
-                Join
-            </button>
-        </div>
-    </div>
-  </main>
+    </main>
 </template>
 
 <script>
-
+import Materialize from "materialize-css"
+import md5 from 'crypto-js/md5';
 export default {
   name: 'App',
-  data: {
+  data() {
+    return {
       ws: null,
       newMsg: '',
       chatContent: '',
       email: null,
       username: null,
       joined: false
+    }
   },
-  created: function() {
+  created() {
       var self = this;
       this.ws = new WebSocket('ws://' + 'localhost:8000' + '/ws');
       this.ws.addEventListener('message', function(args) {
@@ -64,32 +67,32 @@ export default {
       });
   },
   methods: {
-      send() {
-          if (this.newMsg != '') {
-              this.ws.send(
-                  JSON.stringify({
-                          email: this.email,
-                          username: this.username,
-                          message: this.newMsg
-                      }
-                  ));
-              this.newMsg = '';
-          }
-      },
-      join() {
-          if (!this.email) {
-              Materialize.toast('You must enter an email', 2000);
-              return
-          }
-          if (!this.username) {
-              Materialize.toast('You must choose a username', 2000);
-              return
-          }
-          this.joined = true;
-      },
-      gravatarURL: function(email) {
-          return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
+    send() {
+      if (this.newMsg != '') {
+        this.ws.send(
+          JSON.stringify({
+              email: this.email,
+              username: this.username,
+              message: this.newMsg
+            }
+          ));
+        this.newMsg = '';
       }
+    },
+    join() {
+      if (!this.email) {
+        Materialize.toast('You must enter an email', 2000);
+        return
+      }
+      if (!this.username) {
+        Materialize.toast('You must choose a username', 2000);
+        return
+      }
+      this.joined = true;
+    },
+    gravatarURL(email) {
+      return 'http://www.gravatar.com/avatar/' + md5(email);
+    }
   }
 }
 </script>
